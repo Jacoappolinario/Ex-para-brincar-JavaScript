5b83800d34a04781b7cd8378ba03440e3a594be5
Jacó Apolinário <jacoapollinario@gmail.com>
2019-08-28 14:40:40 -0300

    Ex para brincar com JS

---
+++ O operador new/js/Operador.js
@@ -0,0 +1,14 @@
+class Calopsita {
+  constructor(nome, raca) {
+    this.nome = nome;
+    this.raca = raca;
+  }
+
+  voa() {
+    return (this.voar = "Posso voar");
+  }
+
+  anda() {
+    return (this.andar = "posso andar");
+  }
+}
