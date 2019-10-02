ed4ae3536a297df284231136a4d00819f3852797
Jacó Apolinário <jacoapollinario@gmail.com>
2019-10-02 13:15:50 -0300

    Novo Ex

---
+++ prefiro_evitar_fadiga/js/contadorController.js
@@ -0,0 +1,16 @@
+class ContadorController {
+  constructor() {
+    this._contador = 0;
+    this._elemento = document.querySelector("#p1"); // busca uma unica vez
+    alert(this._contador);
+  }
+
+  get contador() {
+    return this._contador;
+  }
+
+  incrementa() {
+    this._contador++;
+    this._elemento.textContent = this._contador; //não precisa buscar o elemento, já temos uma referência para ele
+  }
+}
